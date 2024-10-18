70fb15d823d14c51aadc8dd22450ba096c3eefb4
InduGolluri <127374663+InduGolluri@users.noreply.github.com>
2024-10-18 17:36:31 +0530

    Create model.py

---
+++ labels/model.py
@@ -0,0 +1,16 @@
+from django.db.models import Count, Avg
+from django.shortcuts import render, redirect
+from django.db.models import Count
+from django.db.models import Q
+import datetime
+import xlwt
+from django.http import HttpResponse
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.ensemble import RandomForestClassifier
